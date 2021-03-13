a816b9a4558f1d55872f39e2197023e666d33fd9
LFMP <luizfellipe42@hotmail.com>
2021-03-12 23:03:04 -0300

    fix bug

--- scripts/rsna_extract_images.py
+++ scripts/rsna_extract_images.py
@@ -50,7 +50,7 @@ for target in ["Normal", "No Lung Opacity / Not Normal", "Lung Opacity"]:
   if not os.path.isdir(dest_dir):
     os.makedirs(dest_dir)
 
-  for pid in range(nrows)
+  for pid in range(nrows):
     full_pid = local_df["patientId"][pid]
     dcm_file = os.path.join(extractdir,
                             "stage_2_train_images/%s.dcm" % full_pid)
